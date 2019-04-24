277ec0b21dab621a8ab7eae6bc81a81ec97ba096
Peter Brunetto <brunettodesign@gmail.com>
2019-04-23 20:43:39 -0400

    Accessible WordPress theme utilizing flex layouts and modern development practices.
    Building a first wordpress theme over here.

---
+++ wp-content/themes/brunetto_design/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Brunetto_Design
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'brunetto_design' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'brunetto_design' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'brunetto_design' ), 'brunetto_design', '<a href="https://brunetto.design">Peter Brunetto</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
